79d2c73afdc2dcaf2cfcd8d8ddde31252b85ae17
Marine Dunstetter <marine.dunstetter@simplabs.com>
2022-10-14 09:56:03 +0200

    test: node test should pass if non relative links concern an ember version

--- node-tests/helpers/getNonRelativeGuidesLinks.js
+++ node-tests/helpers/getNonRelativeGuidesLinks.js
@@ -1,7 +1,9 @@
 module.exports = function findBadLineBreaks(filepath, links) {
   let results = [];
+  let includeSemver = false;
   links.forEach(function (link) {
-    if (link.includes('guides.emberjs.com/')) {
+    includeSemver = (/v\d+.\d+.\d+/).test(link);
+    if (link.includes('guides.emberjs.com/') && !includeSemver) {
       results.push({ fileToFix: filepath, makeThisARelativePath: link });
     }
   });
