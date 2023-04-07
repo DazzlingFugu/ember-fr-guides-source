d34ee88a49ae4fb1a41b008b3ffe1e8ff69438b0
Marine Dunstetter <marine.dunstetter@simplabs.com>
2023-04-07 10:09:10 +0200

    update remark packages

---
+++ .remarkrc.mjs
@@ -0,0 +1,34 @@
+/* eslint-env node */
+import { unified } from "unified";
+import remarkRetext from "remark-retext"; 
+
+import retextLatin from "retext-latin";
+import retextRepeatedWords from "retext-repeated-words";
+import retextSyntaxUrls from "retext-syntax-urls"
+import retextSpell from "retext-spell"
+
+import dictionaryFr from "dictionary-fr";
+import { readFileSync } from 'fs';
+
+const remarkConfig = {
+  plugins: [
+    [
+      remarkRetext,
+      unified()
+        .use(retextLatin)
+        .use(retextRepeatedWords)
+        .use(retextSyntaxUrls)
+        .use(retextSpell, {
+          dictionary: dictionaryFr,
+          personal: readFileSync("./.local.dic"),
+        })
+    ],
+    "remark-preset-lint-consistent",
+    "remark-preset-lint-recommended",
+    ["remark-lint-list-item-indent", "space"],
+    ["remark-lint-list-item-bullet-indent", false],
+    ["remark-lint-code-block-style", false],
+  ]
+};
+
+export default remarkConfig;
