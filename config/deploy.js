3bff3974240bdfbac06b9aeafe0dd9f86e39b990
Marine Dunstetter <marine.dunstetter@simplabs.com>
2023-08-11 12:39:29 +0200

    allow search index upload with algolia with ember deploy production

--- config/deploy.js
+++ config/deploy.js
@@ -11,33 +11,32 @@ try {
 
 module.exports = function(deployTarget) {
   let ENV = {
-    build: {},
+    // build: {},
     'prember-algolia': {
       indexName: credentials.algoliaIndex || process.env.ALGOLIA_INDEX,
       applicationId: credentials.algoliaApplication || process.env.ALGOLIA_APPLICATION,
       apiKey: credentials.algoliaKey || process.env.ALGOLIA_KEY,
-      tagsToExclude: '.old-version-warning,.edit-page,code,pre',
+      tagsToExclude: '.edit-page,code,pre,img,div.cta,div.filename',
       cssSelector: 'section',
-      versionsToIgnore: ['1.x', '2.x'],
       versionPattern: /^(v\d+\.\d+\.\d+)\//,
       pathPattern: /^v\d+\.\d+\.\d+\/(.*)\/index.html$/,
     }
   };
 
-  if (deployTarget === 'development') {
-    ENV.build.environment = 'development';
-    // configure other plugins for development deploy target here
-  }
+  // if (deployTarget === 'development') {
+  //   ENV.build.environment = 'development';
+  //   // configure other plugins for development deploy target here
+  // }
 
-  if (deployTarget === 'staging') {
-    ENV.build.environment = 'production';
-    // configure other plugins for staging deploy target here
-  }
+  // if (deployTarget === 'staging') {
+  //   ENV.build.environment = 'production';
+  //   // configure other plugins for staging deploy target here
+  // }
 
-  if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
-  }
+  // if (deployTarget === 'production') {
+  //   ENV.build.environment = 'production';
+  //   // configure other plugins for production deploy target here
+  // }
 
   // Note: if you need to build some configuration asynchronously, you can return
   // a promise that resolves with the ENV object instead of returning the
