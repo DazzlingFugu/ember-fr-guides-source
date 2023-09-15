8aee051e0d54bb5f9bbb4a00aecb5d604daf45c7
Marine Dunstetter <bluecut.official@gmail.com>
2023-09-15 09:26:32 +0200

    uncomment the config lines that had been commented during the set up

--- config/deploy.js
+++ config/deploy.js
@@ -11,7 +11,7 @@ try {
 
 module.exports = function(deployTarget) {
   let ENV = {
-    // build: {},
+    build: {},
     'prember-algolia': {
       indexName: credentials.algoliaIndex || process.env.ALGOLIA_INDEX,
       applicationId: credentials.algoliaApplication || process.env.ALGOLIA_APPLICATION,
@@ -23,20 +23,20 @@ module.exports = function(deployTarget) {
     }
   };
 
-  // if (deployTarget === 'development') {
-  //   ENV.build.environment = 'development';
-  //   // configure other plugins for development deploy target here
-  // }
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
+  }
 
-  // if (deployTarget === 'staging') {
-  //   ENV.build.environment = 'production';
-  //   // configure other plugins for staging deploy target here
-  // }
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
 
-  // if (deployTarget === 'production') {
-  //   ENV.build.environment = 'production';
-  //   // configure other plugins for production deploy target here
-  // }
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+  }
 
   // Note: if you need to build some configuration asynchronously, you can return
   // a promise that resolves with the ENV object instead of returning the
