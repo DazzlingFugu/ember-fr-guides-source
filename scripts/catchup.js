18dc6e3ba01180f5cc08fe8e02dd53b97f818060
Marine Dunstetter <bluecut.official@gmail.com>
2023-11-21 19:11:33 +0100

    feat: split diff into multiple files and apply English automatically

--- scripts/catchup.js
+++ scripts/catchup.js
@@ -1,6 +1,12 @@
 var fs = require('fs');
 var shell = require('shelljs');
 
+// Create a catchup branch out of the current branch (should be up to date master)
+if (shell.exec('git checkout -b catchup').code !== 0) {
+  shell.echo('shelljs: "git checkout -b catchup" command failed');
+  shell.exit(1);
+}
+
 // Fetch the latest updates in the official Ember Guides
 if (shell.exec('git fetch upstream').code !== 0) {
   shell.echo('shelljs: "git fetch upstream" command failed');
@@ -35,5 +41,33 @@ fs.readFile('english.diff', 'utf8', function (error, data) {
 
   fs.writeFile('english.diff', result, 'utf8', function (error) {
      if (error) return console.log(error);
+  });
+});
+
+// Go back to catchup branch where we want to actually apply the diff
+if (shell.exec('git switch catchup').code !== 0) {
+  shell.echo('shelljs: "git switch catchup" command failed');
+  shell.exit(1);
+}
+
+// Separate the diff into multiple files and try to apply
+fs.readFile('english.diff', 'utf8', function (error, data) {
+  if (error) {
+    return console.log(error);
+  }
+  // Consider a line starting with "diff --git b/" should be a diff block
+  let search = /diff --git b\//;
+  let blocks = data.split(search);
+
+  blocks.forEach((block, index) => {
+    if (block.length) {
+      // Create a file scripts/diff-N containing only the current diff block
+      fs.writeFile(`scripts/diff--${index}.diff`, `diff --git b/${block}`, function (err) {
+        if (err) throw err;
+        if (shell.exec(`git apply scripts/diff--${index}.diff`).code !== 0) {
+          shell.echo(`shelljs: "git apply" command failed for diff--${index}.diff`);
+        }
+      });
+    }
   });
 });
